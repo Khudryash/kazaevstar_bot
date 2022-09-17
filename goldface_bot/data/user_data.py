e1d815a37ccfb2b8b3d60187bd707f04191480df
Khudryash <90473852+Khudryash@users.noreply.github.com>
2022-09-17 10:57:55 +0300

    Delete goldface_bot directory

--- goldface_bot/data/user_data.py
+++
@@ -1,9 +0,0 @@
-from aiogram.dispatcher.filters.state import State, StatesGroup
-
-
-# residential
-class Proofs(StatesGroup):
-    screenshot = State()
-    card = State()
-    bank = State()
-    name = State()
--- goldface_bot/handlers/users/__init__.py
+++
@@ -1,9 +0,0 @@
-from .start import dp
-from .cancel import dp
-from .help import dp
-from .kashback import dp
-from .notify_admins import dp
-from .move_to_work import dp
-from .error_feedback import dp
-
-__all__ = ['dp']
